7e8c2bfa724024c28d92d9042dca85ab7c2a0529
YuviPanda <yuvipanda@gmail.com>
2015-10-07 22:50:39 -0700

    dokuwiki: Add sqlite, data, bureaucracy plugins
    Also provision them on hillhackswiki

--- manifests/hillhacks.pp
+++ manifests/hillhacks.pp
@@ -2,7 +2,12 @@ class hillhacks {
     dokuwiki { 'wiki.hillhacks.in':
     }
 
-    dokuwiki::plugin { 'captcha':
+    dokuwiki::plugin { [
+        'sqlite',
+        'data',
+        'bureaucracy',
+        'captcha'
+    ]:
         hostname => 'wiki.hillhacks.in',
         require  => Dokuwiki['wiki.hillhacks.in'],
     }
